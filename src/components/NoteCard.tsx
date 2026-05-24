b3d0ae100be77f71e638b4a77557cd9cde57b9a6
Deeptangshu <duttadeeptangshu@outlook.com>
2026-05-24 18:13:00 +0530

    Fix Delete button UI

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -16,16 +16,16 @@ export default function NoteCard({
 		(state) => state.deleteNote
 	)
 	return (
-		<div className="relative bg-zinc-900 border border-zinc-800 rounded-2xl p-5 hover:border-zinc-500 hover:scale-106 hover:bg-zinc-700 transition">
-			<button onClick={() => deleteNote(id)} className="absolute top-4 right-4 text-zinc-500 hover-text-red-400 transition">
-				<Trash2 size={18}/>
+		<div className="relative bg-zinc-900 border border-zinc-800 rounded-2xl p-5 hover:border-zinc-500 hover:scale-105 hover:bg-zinc-700 transition">
+			<button onClick={() => deleteNote(id)} className="absolute top-4 right-4 text-zinc-500 border border-zinc-500 rounded-full p-2 hover:bg-[#ad2323] hover:text-[#ffffff] hover:border-zinc-400 transition-all duration-300 ease-in-out">
+				<Trash2 size={18} />
 			</button>
-			<h2 className="text-lg front-semibold mb-2">
+			<h2 className="text-lg font-semibold mb-2">
 				{title}
 			</h2>
 			<p className="text-zinc-400 text-sm line-clamp-4">
 				{content}
 			</p>
 		</div>
-	)	
+	)
 }
